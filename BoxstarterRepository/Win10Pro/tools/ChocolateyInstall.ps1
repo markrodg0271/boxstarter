8eb217d038133704d37d415ff040a799742fa945
Mark Rodgers <MRodgers@TC-C02YWBDFLVCG.local>
2020-08-15 10:48:38 -0700

    cortana

--- BoxstarterRepository/Win10Pro/tools/ChocolateyInstall.ps1
+++ BoxstarterRepository/Win10Pro/tools/ChocolateyInstall.ps1
@@ -1,62 +1,88 @@
 
-	# Boxstarter options
-	$Boxstarter.RebootOk=$true # Allow reboots?
-	$Boxstarter.NoPassword=$false
-	$Boxstarter.AutoLogin=$true # Save password securely and auto-login after a reboot
+	# Disable Cortana
+Function DisableCortana {
+    Write-Host "Disabling Cortana..."
+    If (!(Test-Path "HKCU:\SOFTWARE\Microsoft\Personalization\Settings")) {
+        New-Item -Path "HKCU:\SOFTWARE\Microsoft\Personalization\Settings" -Force | Out-Null
+    }
+    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Personalization\Settings" -Name "AcceptedPrivacyPolicy" -Type DWord -Value 0
+    If (!(Test-Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization")) {
+        New-Item -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization" -Force | Out-Null
+    }
+    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization" -Name "RestrictImplicitTextCollection" -Type DWord -Value 1
+    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization" -Name "RestrictImplicitInkCollection" -Type DWord -Value 1
+    If (!(Test-Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization\TrainedDataStore")) {
+        New-Item -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization\TrainedDataStore" -Force | Out-Null
+    }
+    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization\TrainedDataStore" -Name "HarvestContacts" -Type DWord -Value 0
+    If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search")) {
+        New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Force | Out-Null
+    }
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Name "AllowCortana" -Type DWord -Value 0
+}
 
-	# Basic Setup
-	Update-ExecutionPolicy Unrestricted
-	Disable-InternetExplorerESC
-	Disable-UAC
 	
-	Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
+# Boxstarter options
+$Boxstarter.RebootOk=$true # Allow reboots?
+$Boxstarter.NoPassword=$false
+$Boxstarter.AutoLogin=$true # Save password securely and auto-login after a reboot
 
-	Set-StartScreenOptions -EnableListDesktopAppsFirst
+# Basic Setup
+Update-ExecutionPolicy Unrestricted
+Disable-InternetExplorerESC
+Disable-UAC
 
-	Set-StartScreenOptions -EnableBootToDesktop -EnableDesktopBackgroundOnStart -EnableShowStartOnActiveScreen -EnableShowAppsViewOnStartScreen -EnableSearchEverywhereInAppsView -EnableListDesktopAppsFirst
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
 
-	# Disables the Bing Internet Search when searching from the search field in the Taskbar or Start Menu.
-	Disable-BingSearch
-	Disable-GameBarTips
+Set-StartScreenOptions -EnableListDesktopAppsFirst
 
-	# remember to make it rerunnable since this script will execute multiple times
-	if (Test-PendingReboot) { Invoke-Reboot }
+Set-StartScreenOptions -EnableBootToDesktop -EnableDesktopBackgroundOnStart -EnableShowStartOnActiveScreen -EnableShowAppsViewOnStartScreen -EnableSearchEverywhereInAppsView -EnableListDesktopAppsFirst
 
-	cinst notepadplusplus -y
-	cinst google-chrome-x64 -y
-	cinst git -y
-	cinst git-credential-winstore -y
-	cinst fiddler -y
-	cinst curl -y
-	# cinst azurepowershell -y
-	cinst azure-cli -y
-	cinst az.powershell -y
-	cinst azcopy -y
-	cinst powershell-core -y
-	cinst microsoftazurestorageexplorer -y
-	cinst winrar -y
-	cinst vscode -y
-	cinst vscode-powershell -y
-	cinst vscode-icons -y
-	cinst royalts-v5 -y
-	cinst beyondcompare -y
-	cinst dropbox -y
-	cinst openssl.light -y
-	cinst steam -y
+# Disables the Bing Internet Search when searching from the search field in the Taskbar or Start Menu.
+Disable-BingSearch
+Disable-GameBarTips
 
-	# install Windows features
-	cinst TelnetClient -source windowsfeatures -y
-	if (Test-PendingReboot) { Invoke-Reboot }
+# Disable Cortana
+DisableCortana
 
-	#taskbar items
-	Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Google\Chrome\Application\chrome.exe"
-	Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles}\Microsoft VS Code\Code.exe"
-	Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Royal TS V5\RoyalTS.exe"
+# remember to make it rerunnable since this script will execute multiple times
+if (Test-PendingReboot) { Invoke-Reboot }
 
-	# Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-	# Install-Module AzureAD -force
+cinst notepadplusplus -y
+cinst google-chrome-x64 -y
+cinst git -y
+cinst git-credential-winstore -y
+cinst fiddler -y
+cinst curl -y
+# cinst azurepowershell -y
+cinst azure-cli -y
+cinst az.powershell -y
+cinst azcopy -y
+cinst powershell-core -y
+cinst microsoftazurestorageexplorer -y
+cinst winrar -y
+cinst vscode -y
+cinst vscode-powershell -y
+cinst vscode-icons -y
+cinst royalts-v5 -y
+cinst beyondcompare -y
+cinst dropbox -y
+cinst openssl.light -y
+cinst steam -y
 
-	# Update Windows and reboot if necessary
-	Install-WindowsUpdate -AcceptEula
-	if (Test-PendingReboot) { Invoke-Reboot }
+# install Windows features
+cinst TelnetClient -source windowsfeatures -y
+if (Test-PendingReboot) { Invoke-Reboot }
+
+#taskbar items
+Install-ChocolateyPinnedTaskBarItem "$($Boxstarter.programFiles86)\Google\Chrome\Application\chrome.exe"
+Install-ChocolateyPinnedTaskBarItem "$($Boxstarter.programFiles)\Microsoft VS Code\Code.exe"
+Install-ChocolateyPinnedTaskBarItem "$($Boxstarter.programFiles86)\Royal TS V5\RoyalTS.exe"
+
+# Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+# Install-Module AzureAD -force
+
+# Update Windows and reboot if necessary
+Install-WindowsUpdate -AcceptEula
+if (Test-PendingReboot) { Invoke-Reboot }
 
